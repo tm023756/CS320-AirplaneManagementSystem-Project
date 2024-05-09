d2a2a6b2018248208215c7a4d1938cca18b0a91e
tm023756 <teymur.mammadov@ozu.edu.tr>
2024-05-09 22:45:04 +0300

    Add Business, Economy, and FamilyPassenger

---
+++ src/model/passenger/EconomyPassenger.java
@@ -0,0 +1,13 @@
+package model.passenger;
+
+public class EconomyPassenger extends Passenger{
+
+    public EconomyPassenger(String name, String surname, int luggageCount, int yearOfBirth) throws Exception {
+        setName(name);
+        setSurname(surname);
+        setLuggageCount(luggageCount);
+        setYearOfBirth(yearOfBirth);
+        setPassengerType(PassengerType.Economy);
+    }
+
+}
